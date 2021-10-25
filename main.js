fabc92b9239c8813a46d4e27a6da10123f414bd0
fernandohds <fernandofigueroa.hds@gmail.com>
2021-10-24 23:32:12 -0300

    arreglo de api

--- main.js
+++ main.js
@@ -1,26 +1,21 @@
 
-let equipo = prompt("Ingresa uno de los 5 grandes en mayuscula");
-//Repetimos hasta que se ingresa "ESC"
-while(equipo != "ESC" ){
-   switch (equipo) {
-        case "INDEPENDIENTE":
-            alert("CLUB ATLETICO INDEPENDIENTE");
-            break;
-        case "RIVER":
-            alert("CLUB ATLETICO RIVER PLATE");
-            break;
-        case "BOCA":
-                alert("CLUB ATLETICO BOCA JUNIORS");
-            break;
-        case "RACING":
-                alert("CLUB ATLETICO RACING CLUB");
-            break;    
-        case "SAN LORENZO":
-                alert("CLUB ATLETICO SAN LORENZO DE ALMAGRO");
-            break;        
-       default:
-           alert("ESE NO ES UN EQUIPO GRANDE")
-           break;
-   }
-   equipo= prompt("Ingresa otro club");
-}
+const URL = "https://hp-api.herokuapp.com/";
+
+$("body").append(
+    `<div id="personajes" class="btn btn-success col-auto m-1">Personajes</div>`
+);
+
+$("#personajes").click (() => {
+    $.get(`${URL}api/characters`, function (res, state){
+        if (state === "success") {
+            console.log(res);
+            for (const {name, house} of res) {
+                $(".row").append(`
+                <div class="card col-sm-3 m-1">
+                    <div class="h3">${name}</div>
+                    <div>${house}</div>
+                </div>`);
+            }
+        }
+    });
+})
